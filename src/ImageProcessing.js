f6665f57db861b4c8b2311092cd1817575e2a1bf
rizqa <rizqa22si@mahasiswa.pcr.ac.id>
2024-07-09 09:49:55 +0700

    new

--- src/ImageProcessing.js
+++ src/ImageProcessing.js
@@ -12,31 +12,31 @@ export default function ImageProcessing() {
   const labelDescriptions = {
     0: {
       label: "Avulsion Fracture",
-      description: "Patah tulang yang terjadi ketika sepotong kecil tulang terlepas dari bagian utama tulang karena tarikan kuat dari otot atau ligamen yang melekat pada tulang tersebut",
+      description: "Patah tulang yang terjadi ketika sepotong kecil tulang terlepas dari bagian utama tulang karena tarikan kuat dari otot atau ligamen yang melekat pada tulang tersebut.Pengobatan avulsion fracture bervariasi tergantung pada tingkat keparahan cedera. Beberapa kasus mungkin memerlukan hanya istirahat dan imobilisasi, sementara yang lain mungkin memerlukan intervensi bedah.",
     },
     1: {
       label: "Comminuted Fracture",
-      description: "Patah tulang yang terjadi menjadi beberapa bagian kecil atau serpihan.Ini biasanya disebabkan oleh trauma dengan energi tinggi, seperti kecelakaan kendaraan bermotor.",
+      description: "Patah tulang yang terjadi menjadi beberapa bagian kecil atau serpihan.Ini biasanya disebabkan oleh trauma dengan energi tinggi, seperti kecelakaan kendaraan bermotor.Pengobatan comminuted fracture melibatkan imobilisasi menggunakan gips atau belat untuk menjaga fragmen tulang tetap pada posisinya, dan sering kali diperlukan pembedahan untuk menstabilkan fragmen tulang dengan pelat logam, sekrup, atau batang logam. Setelah tulang mulai sembuh, terapi fisik dilakukan untuk memulihkan fungsi dan kekuatan anggota tubuh yang terkena. Traksi mungkin juga digunakan dalam beberapa kasus untuk mengatur fragmen tulang melalui tarikan yang terus-menerus.",
     },
     2: {
       label: "Compression Crush Fracture",
-      description: "Patah tulang ini biasanya terjadi pada tulang belakang (vertebrae) ketika tulang mengalami tekanan yang berlebihan, sehingga tulang tersebut hancur atau tertekan.",
+      description: "Patah tulang ini biasanya terjadi pada tulang belakang (vertebrae) ketika tulang mengalami tekanan yang berlebihan, sehingga tulang tersebut hancur atau tertekan.Pengobatan compression crush fracture melibatkan istirahat dan imobilisasi untuk mengurangi tekanan pada tulang yang terkena, penggunaan brace atau korset untuk mendukung tulang belakang, serta medikasi untuk mengelola nyeri dan menguatkan tulang dengan suplemen kalsium dan vitamin D. Terapi fisik diperlukan untuk memperkuat otot-otot penyangga dan meningkatkan fleksibilitas. Dalam kasus yang parah, pembedahan seperti vertebroplasty atau kyphoplasty mungkin diperlukan untuk menstabilkan tulang belakang dan mengurangi nyeri.",
     },
     3: {
       label: "Fracture Dislocation",
-      description: "Jenis ini terjadi ketika tulang patah dan sendi terlepas dari tempatnya. Ini adalah kombinasi antara patah tulang dan dislokasi sendi.",
+      description: "Jenis ini terjadi ketika tulang patah dan sendi terlepas dari tempatnya. Ini adalah kombinasi antara patah tulang dan dislokasi sendi.Pengobatan fracture dislocation biasanya memerlukan reposisi (mengembalikan posisi tulang dan sendi ke tempatnya semula), imobilisasi, dan dalam beberapa kasus, pembedahan. Proses pemulihan juga sering melibatkan fisioterapi untuk memulihkan fungsi sendi dan kekuatan otot.",
     },
     4: {
       label: "Longitudinal Fracture",
-      description: "Pada patah tulang ini, garis patahan terjadi sejajar dengan sumbu panjang tulang. Ini sering kali terjadi akibat trauma langsung pada tulang.",
+      description: "Pada patah tulang ini, garis patahan terjadi sejajar dengan sumbu panjang tulang. Ini sering kali terjadi akibat trauma langsung pada tulang.Pengobatan longitudinal fracture melibatkan imobilisasi dengan gips atau belat untuk menjaga fragmen tulang tetap pada posisinya dan memungkinkan penyembuhan. Obat pereda nyeri dapat diberikan untuk mengelola rasa sakit, dan terapi fisik mungkin diperlukan setelah penyembuhan awal untuk memulihkan kekuatan dan fungsi anggota tubuh yang terkena.",
     },
     5: {
       label: "Oblique Fracture",
-      description: "Jenis ini terjadi ketika garis patahan membentuk sudut terhadap sumbu panjang tulang. Ini bisa terjadi akibat trauma dari sudut atau kekuatan rotasional.",
+      description: "Jenis ini terjadi ketika garis patahan membentuk sudut terhadap sumbu panjang tulang. Ini bisa terjadi akibat trauma dari sudut atau kekuatan rotasional.Pengobatan oblique fracture melibatkan reposisi tulang yang patah (jika diperlukan) dan imobilisasi menggunakan gips atau belat.",
     },
     6: {
       label: "Spiral Fracture",
-      description: "Patah tulang ini memiliki garis patahan yang melingkar atau berputar di sepanjang tulang. Biasanya disebabkan oleh gaya torsi atau putaran yang kuat pada tulang, seperti yang mungkin terjadi pada kecelakaan olahraga atau aktivitas fisik berat lainnya",
+      description: "Patah tulang ini memiliki garis patahan yang melingkar atau berputar di sepanjang tulang. Biasanya disebabkan oleh gaya torsi atau putaran yang kuat pada tulang, seperti yang mungkin terjadi pada kecelakaan olahraga atau aktivitas fisik berat lainnya.Pengobatan spiral fracture sering kali memerlukan reposisi tulang yang patah dan imobilisasi dengan gips atau belat. Jika fragmen tulang tidak stabil, pembedahan dengan pelat logam atau sekrup mungkin diperlukan. Setelah fase awal penyembuhan, terapi fisik dilakukan untuk mengembalikan kekuatan dan fungsi anggota tubuh yang terkena.",
     },
   };
 
@@ -49,7 +49,7 @@ export default function ImageProcessing() {
 
     try {
       const response = await axios.post(
-        "https://6d33-104-196-2-64.ngrok-free.app/predict",
+        "https://b2f9-34-23-244-11.ngrok-free.app/predict",
         formData,
         {
           headers: {
@@ -110,6 +110,9 @@ export default function ImageProcessing() {
                 {description}
               </p>
               <p className="text-black-500 mb-4 text-left">
+                <div className="text-2xl font-bold font-dm-serif-text text-[#1E174C]">
+                  Akurasi
+                </div>
                 {confidence}
               </p>
             </>
